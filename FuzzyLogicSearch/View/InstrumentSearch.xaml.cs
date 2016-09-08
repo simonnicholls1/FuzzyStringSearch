50c4225b355dc05988951a08405b91f31dfb7d25
Simon Nicholls <simonnicholls1@gmail.com>
2016-09-08 20:06:03 +0100

    first commit

---
+++ FuzzyLogicSearch/View/InstrumentSearch.xaml.cs
@@ -0,0 +1,20 @@
+﻿using FuzzyLogicSearch.ViewModel;
+using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace FuzzyLogicSearch
+{
+    /// <summary>
+    /// Interaction logic for InstrumentSearch.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InstrumentSearchViewModel viewModel = new InstrumentSearchViewModel();            
+
+            this.DataContext = viewModel;
+            InitializeComponent();
+        }
+    }
+}
